6e9049f359dfb5ced6865526d9956a319ebdfbee
Alejandro Maeso Aguilera <93497327+alejandromsagui@users.noreply.github.com>
2022-01-30 21:03:32 +0100

    Add files via upload

---
+++ Account/src/test/java/es/iessoterohernandez/daw/endes/Account/AccountTest.java
@@ -0,0 +1,39 @@
+package es.iessoterohernandez.daw.endes.Account;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+	static Account cuenta;
+	
+	@BeforeEach
+	public void inicio() {
+		cuenta = new Account("Alejandro", 45321, 33234);
+	}
+	
+	@Test
+	public void depositTest() {
+		assertEquals(true, cuenta.deposit(48750));
+		assertEquals(false, cuenta.deposit(-1));
+	}
+	
+	@Test
+	public void withdrawTest() {
+		assertEquals(false, cuenta.withdraw(-1, -2));
+		assertEquals(true, cuenta.withdraw(20, 30));
+	}
+	
+	@Test
+	public void addInterestTest() {
+		assertEquals(33234, cuenta.getBalance());
+	}
+	
+	@Test 
+	public void toStringTest() {
+		String esperado = cuenta.getAccountNumber()+"Alejandro" + +cuenta.getBalance();
+		assertEquals(esperado, cuenta.toString());
+	}
+}
