c3670038db12b179ee5496f068853a610937d94e
Alejandro Maeso Aguilera <93497327+alejandromsagui@users.noreply.github.com>
2022-02-03 10:03:54 +0100

    Delete src directory

--- src/main/java/es/iessoterohernandez/daw/endes/PruebaJunit/IngresoNegativoException.java
+++
@@ -1,7 +0,0 @@
-package es.iessoterohernandez.daw.endes.PruebaJunit;
-
-public class IngresoNegativoException extends Exception{
-	public IngresoNegativoException(String message) {
-		super(message);
-	}
-}
